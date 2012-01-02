66d3e6c139f9e7655c2ea3c8e611ed53bd8909df
root <root@localhost.localdomain>
2012-01-02 21:16:59 +0800

    change dir name.

---
+++ learn_makefile/main1.cpp
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:    main1.cpp
+ *       Author:      linwei
+ *       E-mail:      kinglw8729@gmail.com
+ *       Created:     2011年12月31日 11时44分13秒
+ *       Compiler:    gcc
+ *       Description: 
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+int main()
+{
+  std::cout<<"Hello world!"<<std::endl;
+  return 0;
+}
