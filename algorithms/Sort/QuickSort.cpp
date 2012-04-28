1c10ad253815ff8e23b4b862c54c16616a56e9ba
unknown <linv@linv-PC.(none)>
2012-04-29 01:02:54 +0800

    finish quicksort.

--- algorithms/Sort/QuickSort.cpp
+++ algorithms/Sort/QuickSort.cpp
@@ -15,26 +15,35 @@ void ExChange(int &a, int &b){
     a = a^b;
 }
 
-int Partition(int *array, const int left, const int right){
-    cout<<"left:"<<left<<" right:"<<right<<endl;
-    int pivot = array[right];
-    int i = left;
-    int j = 0;
-    for( j = right - 1; j >= i;){
-        if(array[i] <= pivot){
-            ++i;
-            continue;
+int Partition(int array[], const int left, const int right){
+    int tmp = array[right];
+    int l = left;
+    int r = right-1;
+    while(l<=r){
+        if(array[l]>tmp){
+            if(l!=r)
+                ExChange(array[l],array[r]);
+            --r;
         }
-        if(array[j] >= pivot){
-            --j;
-            continue;
+        else if(array[r]<tmp){
+            if(l!=r)
+                ExChange(array[l],array[r]);
+            ++l;
         }
-        ExChange(array[i+1], array[j]);
-        i++;
-        j--;
+        else{
+            ++l;
+            --r;
+        }
     }
-    ExChange(array[i], array[right]);
-    return i;
+    if(l!=r){
+        array[right] = array[l];
+        array[l] = tmp;
+        return l;
+    }
+    else {
+        ExChange(array[r],array[right]);
+        return r;
+    }
 }
 
 void QuickSort(int *array, const int left, const int right){
@@ -52,10 +61,10 @@ int main()
     int array[10] = {0,1,2,3,4,5,6,7,8,9};
     int array1[10] = {9,8,7,6,5,4,3,2,1,0};
     int array2[10] = {6,8,2,9,5,0,3,7,1,4};
-    QuickSort(array,0,9);
+    QuickSort(array1,0,9);
     QuickSort(array2,0,9);
     for(int i = 0; i<10; ++i)
-        cout<<array2[i]<<endl;
+        cout<<array1[i]<<endl;
 //    for(int i = 0; i<10; ++i)
 //        cout<<array[i]<<endl;
     return 0;
